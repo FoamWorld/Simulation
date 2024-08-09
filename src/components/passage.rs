bc027e979c0418951ae0b72140a402a32f742835
Rratic <Rratic@163.com>
2024-08-09 16:17:43 +0800

    fix passage

--- src/components/passage.rs
+++ src/components/passage.rs
@@ -1,27 +1,69 @@
 use crate::semiology::referent::{Referent, Void};
-use std::{cell::RefCell, rc::Rc};
+use std::{borrow::Borrow, cell::RefCell, rc::Rc};
 
-trait Generator {
-    fn generate(&mut self) -> &dyn Referent;
+// trait FnGenerate = Fn(&mut BTreeMap<String, &dyn Any>) -> Rc<RefCell<dyn Referent>>;
+
+pub struct Generator<T>
+where
+    T: Fn() -> Rc<RefCell<dyn Referent>>,
+{
+    pub generator: T,
 }
-
-struct GeneratorTerminus {}
-impl Generator for GeneratorTerminus {
-    fn generate(&mut self) -> &dyn Referent {
-        return &Void {};
+impl<T> Generator<T>
+where
+    T: Fn() -> Rc<RefCell<dyn Referent>>,
+{
+    fn new(gen: T) -> Self {
+        return Generator { generator: gen };
+    }
+    fn generate(&self) -> Rc<RefCell<dyn Referent>> {
+        return (self.generator)();
     }
 }
+impl<T> Referent for Generator<T> where T: Fn() -> Rc<RefCell<dyn Referent>> {}
 
-pub struct Passage {
-    pub locked: Option<u32>,
+pub struct Passage<T>
+where
+    T: Fn() -> Rc<RefCell<dyn Referent>>,
+{
     pub link_src: Rc<RefCell<dyn Referent>>,
     pub link_dest: Rc<RefCell<dyn Referent>>,
+    pub gen_dest: Option<Generator<T>>,
 }
-impl Passage {
-    fn pass(&mut self) {
+impl<T> Passage<T>
+where
+    T: Fn() -> Rc<RefCell<dyn Referent>>,
+{
+    fn new(src: Rc<RefCell<dyn Referent>>, gen: Generator<T>) -> Self {
+        Self {
+            link_src: src,
+            link_dest: Rc::new(RefCell::new(Void {})),
+            gen_dest: Some(gen),
+        }
+    }
+    fn portal(src: Rc<RefCell<dyn Referent>>, dest: Rc<RefCell<dyn Referent>>) -> Self {
+        Self {
+            link_src: src,
+            link_dest: dest,
+            gen_dest: None,
+        }
+    }
+    fn pass(&mut self, uncached: bool) -> Rc<RefCell<dyn Referent>> {
+        if uncached {
+            match self.gen_dest.borrow() {
+                Some(x) => {
+                    self.link_dest = x.generate();
+                }
+                None => return Rc::new(RefCell::new(Void {})),
+            }
+        }
+        return self.link_dest.clone();
     }
 }
-impl Referent for Passage {
+impl<T> Referent for Passage<T>
+where
+    T: Fn() -> Rc<RefCell<dyn Referent>>,
+{
     fn is_interactive(&self) -> bool {
         return true;
     }
