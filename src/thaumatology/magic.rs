f4d47905b992c98659de3a9abfbe96b64bf67b00
Rratic <Rratic@163.com>
2024-10-02 19:12:02 +0800

    better CircleSrc management

--- src/thaumatology/magic.rs
+++ src/thaumatology/magic.rs
@@ -1,18 +1,45 @@
-use super::rune::{EveFlux, Rune};
-use bevy_ecs::entity::Entity;
-use std::cell::RefCell;
+use super::rune::{ComposedConcept, EveFlux, Rune};
+use std::cell::{Ref, RefCell};
 
 struct CircleSrc {
-    nodes: Vec<Entity>,
-    edges: Vec<Fuse>,
+    nodes: Vec<Rune>,
+    nexts: Vec<Vec<usize>>,
+    edges: Vec<Vec<Fuse>>,
+}
+impl CircleSrc {
+    pub fn insert_node(self: &mut Self, node: Rune) {
+        self.nodes.push(node);
+    }
+    pub fn insert_edge(self: &mut Self, from: usize, to: usize, edge: Fuse) {
+        self.nexts[from].push(to);
+        self.edges[from].push(edge);
+    }
+    pub fn start(self: &mut Self, from: usize, flux: &mut EveFlux, istream: &mut ComposedConcept) {
+        self.nodes[from].activate(flux, istream);
+        let len = self.nexts[from].len();
+        for i in 0..len {
+            let mut fuse = self.edges[from][i];
+            let mut branch_flux = EveFlux {
+                flux: flux.flux / (len as f32),
+            };
+            fuse.spark(&mut branch_flux);
+            let next = self.nexts[from][i];
+            self.start(next, &mut branch_flux, &mut istream.clone());
+        }
+    }
 }
 
+#[derive(Clone, Copy)]
 struct Fuse {
-    length: u64,
-    resist: u64,
-    radiate: u64,
-    collected: u64,
+    length: f32,
+    resist: f32,
+    radiate: f32,
+    collected: f32,
 }
 impl Fuse {
-    fn spark(&mut self, flux: u64) {}
+    fn spark(&mut self, flux: &mut EveFlux) {
+        let delta = self.radiate * self.collected - self.resist;
+        flux.flux += delta;
+        self.collected -= delta;
+    }
 }
--- src/thaumatology/rune.rs
+++ src/thaumatology/rune.rs
@@ -1,10 +1,11 @@
 use bevy_ecs::{component::Component, entity::Entity};
+use std::cell::RefCell;
 
 pub struct EveFlux {
-    flux: f32,
+    pub flux: f32,
 }
 
-#[derive(Component)]
+#[derive(Component, Clone)]
 pub struct ComposedConcept {
     pool: Vec<String>,
 }
@@ -16,18 +17,25 @@ impl ComposedConcept {
     }
 }
 
-pub trait Rune {
-    fn activate(&self, flux: &mut EveFlux, istream: ComposedConcept) -> ComposedConcept;
-    // fn next(&self, from: f32, edges: Vec<u32>) {}
+pub struct Rune {
+    device: Box<RefCell<dyn ThaumDevice>>,
+}
+impl Rune {
+    pub fn activate(&self, flux: &mut EveFlux, istream: &mut ComposedConcept) {
+        self.device.borrow_mut().activate(flux, istream);
+    }
+}
+
+pub trait ThaumDevice {
+    fn activate(&self, flux: &mut EveFlux, istream: &mut ComposedConcept);
 }
 
 struct ChargerSimple {
     speed: f32,
 }
-impl Rune for ChargerSimple {
-    fn activate(&self, flux: &mut EveFlux, _: ComposedConcept) -> ComposedConcept {
+impl ThaumDevice for ChargerSimple {
+    fn activate(&self, flux: &mut EveFlux, istream: &mut ComposedConcept) {
         flux.flux += self.speed;
-        return ComposedConcept::new();
     }
 }
 
